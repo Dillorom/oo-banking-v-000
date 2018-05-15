e93ce7e95fe1031ea7508408972fe8a68339b3da
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-15 21:04:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
     @status = "complete"
     else valid? == false
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
